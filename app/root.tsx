4cb370c7ff53808197cd3ab8494b9145aee5c9a4
repository <41586666+repository@users.noreply.github.com>
2023-01-20 20:08:28 +0000

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,47 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import mapboxStyles  from 'mapbox-gl/dist/mapbox-gl.css';
+import localStyles from "./styles.css"
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: mapboxStyles,
+    },
+    {
+      rel: "stylesheet",
+      href: localStyles,
+    }
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
